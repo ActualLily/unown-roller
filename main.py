a9a5c47812de11c968020e89dfc0109ba18b1aa9
Leah <cearule@gmail.com>
2022-12-15 15:49:46 +0100

    Add chancedice

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-import rolldice
 
 import discord
+import rolldice
 from discord import app_commands
 from dotenv import load_dotenv
 
@@ -22,10 +22,12 @@ async def on_ready():
     print("Ready!")
 
 
+# Simple package call + return
 @tree.command(name="roll", description="Roll dice as defined by CritDice syntax (See /syntax)", guild=discord.Object(
     id=GUILD_TEST))
 async def _roll(interaction, diceroll: str, hidden: bool = False):
     result, explanation = rolldice.roll_dice(diceroll)
+    explanation = explanation.replace(",", ", ")
 
     await interaction.response.send_message(
         embed=formatting.embeds.rolls(diceroll, result, explanation, interaction.user),
@@ -34,36 +36,39 @@ async def _roll(interaction, diceroll: str, hidden: bool = False):
 
 @tree.command(name="pokeroll", description="Roll d6 as defined by CritDice syntax and count successes (See /syntax)",
               guild=discord.Object(
-                id=GUILD_TEST))
-async def _pokeroll(interaction, diceroll: str, hidden: bool = False, status: bool = False):
+                  id=GUILD_TEST))
+async def _pokeroll(interaction, diceroll: str, chancedice: bool = False, hidden: bool = False):
     result, explanation = rolldice.roll_dice(diceroll)
+    explanation = explanation.replace(",", ", ")
 
-    if "d6" not in diceroll.replace(" ", ""):
+    # I could technically allow non-D6 rolls here, but this is for Pokérole, which only uses d6.
+    if "d6" not in diceroll.replace(" ", "").lower():
         await interaction.response.send_message(
             embed=formatting.embeds.error("Not valid d6 rolls! (try `/syntax`)", interaction.user),
             ephemeral=True
         )
 
     else:
-        successRolls = [4, 5, 6]
-
-        if (status):
-            successRolls = 6
-
+        # In Pokérole, 4/5/6 is a success.
+        # If you are however rolling for status effects ("Chance Dice"), only a 6 is a success.
+        successRolls = [6] if chancedice else [4, 5, 6]
         totalSuccesses = 0
 
         for success in successRolls:
             totalSuccesses += explanation.count(str(success))
             explanation = explanation.replace(str(success), f"**{success}**")
 
+        # Add -es if plural is required
         if totalSuccesses == 1:
             result = f"{str(totalSuccesses)} success!"
         else:
             result = f"{str(totalSuccesses)} successes!"
 
         await interaction.response.send_message(
-            embed=formatting.embeds.rolls(diceroll, result, explanation, interaction.user),
+            embed=formatting.embeds.rolls(diceroll.lower() + (" chance dice" if chancedice else ""), result,
+                                          explanation, interaction.user),
             ephemeral=hidden
         )
+
 
 client.run(TOKEN)
