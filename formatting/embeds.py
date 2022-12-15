17ff04adec9f4803ee3b770ef4ba10f55deb8fd7
Leah <cearule@gmail.com>
2022-12-15 14:36:36 +0100

    add error message

--- formatting/embeds.py
+++ formatting/embeds.py
@@ -1,27 +1,13 @@
-from typing import Any
-
-import dice
 import discord
 
 
-def rolls(roll, *author):
-    result = dice.roll(roll)
-
-    resultString = ""
-    successes = 0
-
-    for roll in result:
-        resultString += ("[**" + str(roll) + "**] ")
-        if roll > 3:
-            successes += 1
-
-    if successes != 1:
-        successes = str(successes) + " successes!"
-    else:
-        successes = str(successes) + " success!"
-
-    if author is not None:
-        embed = discord.Embed(description=f"` {successes} ` ⟵  {strip(resultString)}", color=message.author.color)
-        embed.set_author(name=f"{author} rolls {roll}!",
+def rolls(roll: str, result: str, explanation: str, author: discord.User):
+    embed = discord.Embed(description=f"` {result} ` ⟵  {explanation}", color=author.color)
+    embed.set_author(name=f"{author} rolls {roll}!",
                      icon_url=author.avatar.url)
-    pass
+
+    return embed
+
+def error(message: str, author: discord.User):
+    embed = discord.Embed(description=f"{message}", color=0xff0000)
+    embed.set_author(name=f"{author} caused an error!")
