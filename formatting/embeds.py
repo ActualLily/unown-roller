def9028f824aa37f5f98e617e454bee9b6c2870f
Leah <cearule@gmail.com>
2022-12-15 15:05:08 +0100

    Add success counting for Pokéroll

--- formatting/embeds.py
+++ formatting/embeds.py
@@ -11,3 +11,5 @@ def rolls(roll: str, result: str, explanation: str, author: discord.User):
 def error(message: str, author: discord.User):
     embed = discord.Embed(description=f"{message}", color=0xff0000)
     embed.set_author(name=f"{author} caused an error!")
+
+    return embed
--- main.py
+++ main.py
@@ -24,29 +24,46 @@ async def on_ready():
 
 @tree.command(name="roll", description="Roll dice as defined by CritDice syntax (See /syntax)", guild=discord.Object(
     id=GUILD_TEST))
-async def _roll(interaction, diceroll: str):
+async def _roll(interaction, diceroll: str, hidden: bool = False):
     result, explanation = rolldice.roll_dice(diceroll)
 
     await interaction.response.send_message(
-        embed=formatting.embeds.rolls(diceroll, result, explanation, interaction.user))
+        embed=formatting.embeds.rolls(diceroll, result, explanation, interaction.user),
+        ephemeral=hidden)
 
 
 @tree.command(name="pokeroll", description="Roll d6 as defined by CritDice syntax and count successes (See /syntax)",
               guild=discord.Object(
-                  id=GUILD_TEST))
-async def _pokeroll(interaction, diceroll: str):
+                id=GUILD_TEST))
+async def _pokeroll(interaction, diceroll: str, hidden: bool = False, status: bool = False):
     result, explanation = rolldice.roll_dice(diceroll)
 
     if "d6" not in diceroll.replace(" ", ""):
         await interaction.response.send_message(
-            embed=formatting.embeds.error("Not valid d6 rolls!", author=interaction.user)
+            embed=formatting.embeds.error("Not valid d6 rolls! (try `/syntax`)", interaction.user),
+            ephemeral=True
         )
 
-    else :
+    else:
+        successRolls = [4, 5, 6]
+
+        if (status):
+            successRolls = 6
+
+        totalSuccesses = 0
+
+        for success in successRolls:
+            totalSuccesses += explanation.count(str(success))
+            explanation = explanation.replace(str(success), f"**{success}**")
+
+        if totalSuccesses == 1:
+            result = f"{str(totalSuccesses)} success!"
+        else:
+            result = f"{str(totalSuccesses)} successes!"
+
         await interaction.response.send_message(
-            embed=formatting.embeds.rolls(diceroll, result, explanation, interaction.user)
+            embed=formatting.embeds.rolls(diceroll, result, explanation, interaction.user),
+            ephemeral=hidden
         )
-
-
 
 client.run(TOKEN)
